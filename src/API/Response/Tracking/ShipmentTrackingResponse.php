ba88575c6266507c179e68e92803203cf0e491f0
Omar Ehab <topdollar57@gmail.com>
2021-10-13 20:33:49 +0200

    tracking issue

--- src/API/Response/Tracking/ShipmentTrackingResponse.php
+++ src/API/Response/Tracking/ShipmentTrackingResponse.php
@@ -44,7 +44,7 @@ class ShipmentTrackingResponse extends Response
     protected function parse($obj): ShipmentTrackingResponse
     {
         parent::parse($obj);
-        dd($obj->TrackingResults);
+        dd($obj);
         if ($result = $obj->TrackingResults->KeyValueOfstringArrayOfTrackingResult) {
             $this->addResult(TrackingResult::parse($result));
         }
